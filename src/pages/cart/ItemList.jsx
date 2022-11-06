e347a7a3c4528baef94cdff8f11e13f415676f16
davidmedev <davidcatrielmamaniescalera@gmail.com>
2022-11-05 21:54:18 -0300

    added helper function to item list component

--- src/pages/cart/ItemList.jsx
+++ src/pages/cart/ItemList.jsx
@@ -4,7 +4,7 @@ import { Button } from "../.././components/button/Button"; // COMPONENT
 import { useContext } from "react"; // HOOKS
 
 const ItemList = () => {
-    const { cart, quantityItems, totalPrice } = useContext(CartContext); // HELPERS
+    const { cart, removeCart, totalQuantityItems, totalPrice } = useContext(CartContext); // HELPERS
 
     const items = cart.map((cadaItem) => <Item key={cadaItem.id} {...cadaItem} />);
 
@@ -13,15 +13,23 @@ const ItemList = () => {
             {/* COMPONENT ITEM */}
             {items}
             <div className="cart-options">
+                {/* COMPONENT BUTTON */}
+                <Button className="cart-empty" onClick={removeCart}>
+                    empty cart
+                </Button>
+                {/* DISCOUNT */}
                 <div className="cart-discount">
                     <input className="cart-cupon" type="text" name="cupon" id="cupon" placeholder="promo code" />
+                    {/* COMPONENT BUTTON */}
                     <Button className="cart-apply">apply</Button>
                 </div>
+                {/* DETAILS */}
                 <article className="cart-details">
                     <h2 className="cart-total">total</h2>
-                    <span className="cart-description">({quantityItems()}) products</span>
+                    <span className="cart-description">({totalQuantityItems()}) products</span>
                     <span className="cart-price">{totalPrice()} USD</span>
                 </article>
+                {/* COMPONENT BUTTON */}
                 <Button>checkout</Button>
             </div>
         </div>
---
+++ src/pages/cart/ItemListCart/ItemCount.jsx
@@ -0,0 +1,37 @@
+import { CartContext } from "../../../context/cart/CartContext"; // CONTEXT
+import { useContext } from "react"; // HOOKS
+
+const ItemCount = ({ itemId, itemQuantity, itemStock }) => {
+    const { decreaseItem, increaseItem, deleteItem } = useContext(CartContext); // HELPERS
+
+    const handleDecrease = () => itemQuantity > 1 && decreaseItem(itemId); // EVENT
+
+    const handleIncrease = () => itemQuantity < itemStock && increaseItem(itemId); // EVENT
+
+    return (
+        <div className="cart-count">
+            {/* DECREASE */}
+            <button onClick={handleDecrease} className="cart-count-btn">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                    <path d="M432 256c0 17.7-14.3 32-32 32L48 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l352 0c17.7 0 32 14.3 32 32z" />
+                </svg>
+            </button>
+            {/* QUANTITY */}
+            <span className="cart-count-quantity">{itemQuantity}</span>
+            {/* INCREASE */}
+            <button onClick={handleIncrease} className="cart-count-btn">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                    <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
+                </svg>
+            </button>
+            {/* DELETE */}
+            <button onClick={() => deleteItem(itemId)} className="cart-count-btn">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                    <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" />
+                </svg>
+            </button>
+        </div>
+    );
+};
+
+export { ItemCount };
