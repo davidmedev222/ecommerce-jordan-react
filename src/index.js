23f84c23a65d59144c2a1fc5cedb140a3a66c83b
davidmedev <davidcatrielmamaniescalera@gmail.com>
2022-11-11 18:31:54 -0300

    added firebase and firestore

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from "./services/firebase/config";
 import { App } from "./router/Router";
+
+initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
