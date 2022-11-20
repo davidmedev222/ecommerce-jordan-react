61858fd146008650ed78631ad2e7b7b6bb04f516
davidmedev <davidcatrielmamaniescalera@gmail.com>
2022-11-20 00:00:04 -0300

    added custom hook

---
+++ src/hooks/firestore/useOrder.jsx
@@ -0,0 +1,39 @@
+import { addDoc, collection, getFirestore, serverTimestamp } from "firebase/firestore"; // FIRESTORE
+import { UserContext } from "../../context/user/UserContext"; // CONTEXT
+import { CartContext } from "../../context/cart/CartContext"; // CONTEXT
+import { useContext } from "react"; // HOOKS
+
+const useOrder = () => {
+    const { user } = useContext(UserContext); // HELPERS
+    const { order, updateOrderUid } = useContext(CartContext); // HELPERS
+
+    const createOrder = (items, totalQuantityItems, totalPrice) => {
+        return {
+            buyer: {
+                name: user.name ?? "",
+                email: user.email ?? "",
+                phone: user.phone ?? "",
+            },
+            items: [...items],
+            total: {
+                items: totalQuantityItems(),
+                price: totalPrice(),
+            },
+            date: serverTimestamp(),
+        };
+    };
+
+    const postOrder = async () => {
+        try {
+            const database = getFirestore();
+            const request = await addDoc(collection(database, "orders"), order);
+            updateOrderUid(request.id);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return { createOrder, postOrder };
+};
+
+export { useOrder };
