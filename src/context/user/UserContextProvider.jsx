77ba172390e3defa12feef6e09a503c9ddfdd515
davidmedev <davidcatrielmamaniescalera@gmail.com>
2022-11-23 23:31:06 -0300

    added user status and helpers to user context

--- src/context/user/UserContextProvider.jsx
+++ src/context/user/UserContextProvider.jsx
@@ -1,26 +1,46 @@
 import { UserContext } from "./UserContext"; // COMPONENT CONTEXT
-import { createUserWithEmailAndPassword } from "firebase/auth"; // AUTH FIREBASE
+import {
+    createUserWithEmailAndPassword,
+    onAuthStateChanged,
+    signInWithEmailAndPassword,
+    signOut,
+    updateProfile,
+} from "firebase/auth"; // AUTH FIREBASE
 import { auth } from "../../services/firebase/config"; // AUTH FIREBASE
-import { useState } from "react"; // HOOKS
+import { useState, useEffect } from "react"; // HOOKS
 
 const UserContextProvider = ({ children }) => {
-    const [user, updateUser] = useState(true); // STATE
+    const [user, updateUser] = useState(false); // STATE
 
-    const [userModel, updateUserModel] = useState({
-        name: "",
-        phone: "",
-        email: "",
-        password: "",
-    }); // MODEL USER
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                // console.log(user);
+                updateUser(user);
+            } else {
+                updateUser(null);
+                // console.log(user);
+            }
+        });
+    }, []);
 
     const createUser = (email, password) => createUserWithEmailAndPassword(auth, email, password);
+
+    const signInUser = (email, password) => signInWithEmailAndPassword(auth, email, password);
+
+    const signOutUser = () => signOut(auth);
+
+    const updateProfileUser = () => {
+        updateProfile(auth.currentUser);
+    };
 
     const values = {
         user,
         updateUser,
-        userModel,
-        updateUserModel,
         createUser,
+        signInUser,
+        signOutUser,
+        updateProfileUser,
     };
 
     return <UserContext.Provider value={values}>{children}</UserContext.Provider>;
