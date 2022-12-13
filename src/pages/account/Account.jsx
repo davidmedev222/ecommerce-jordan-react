d8026e72f394b6483d18541a7b603a2042f06e80
davidmedev <davidmedev@gmail.com>
2022-12-13 00:14:44 -0300

    deleted navbar component in account, error components

--- src/pages/account/Account.jsx
+++ src/pages/account/Account.jsx
@@ -1,12 +1,11 @@
-import { NavBar } from '../../components/navbar/NavBar'; // COMPONENT
 import { Panel } from '../../components/panel/Panel'; // COMPONENT
 
 const Account = () => {
     return (
-        <>
-            <NavBar /> {/* COMPONENT NAVBAR */}
-            <Panel /> {/* COMPONENT PANEL */}
-        </>
+        <main>
+            {/* COMPONENT */}
+            <Panel />
+        </main>
     );
 };
 
--- src/pages/error/Error.jsx
+++ src/pages/error/Error.jsx
@@ -1,9 +1,6 @@
-import { NavBar } from '../../components/navbar/NavBar'; // COMPONENT
-
 const Error = () => {
     return (
         <>
-            <NavBar /> {/* COMPONENT NAVBAR */}
             <h1 className="h1">error 404, pagina no encontrada</h1>
         </>
     );
