d25e60a99d7267fae6231800a52f7c7232b2648d
davidmedev <davidmedev@gmail.com>
2022-12-13 00:12:35 -0300

    updated item list cart  component code and styles

--- src/pages/cart/ItemListCart/ItemListCart.jsx
+++ src/pages/cart/ItemListCart/ItemListCart.jsx
@@ -1,5 +1,4 @@
 import { CartContext } from '../../.././context/cart/CartContext'; // CONTEXT
-import { NavBar } from '../../.././components/navbar/NavBar'; // COMPONENT
 import { ItemList } from './ItemList'; // COMPONENT
 import { EmptyCart } from '.././EmptyCart/EmptyCart'; // COMPONENT
 import { useContext } from 'react'; // HOOKS
@@ -8,16 +7,14 @@ const ItemListCart = () => {
     const { cart } = useContext(CartContext); // HELPERS
 
     return (
-        <>
-            {/* COMPONENT NAV BAR */}
-            <NavBar />
+        <main className="main-cart">
             <section className="cart">
                 {/* TITLE */}
                 {cart.length > 0 && <h1 className="h1">my cart</h1>}
                 {/* COMPONENT ITEM LIST OR EMPTY CART */}
                 {cart.length > 0 ? <ItemList /> : <EmptyCart />}
             </section>
-        </>
+        </main>
     );
 };
 
