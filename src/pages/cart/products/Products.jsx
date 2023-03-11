7636cb7d8bbc1a14b563ffbc9192bd1b452ec7b8
davidmedev222 <davidmedev@gmail.com>
2023-03-10 22:57:55 -0300

    feat(web): add interface of the details for the products in the cart page

--- src/pages/cart/products/Products.jsx
+++ src/pages/cart/products/Products.jsx
@@ -1,26 +1,54 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import { Product } from './Product'
 
-const SectionStyled = styled('section')`
-  padding: clamp(1rem, 1vw, 2rem) clamp(1rem, 4vw, 8rem);
+const ProductsStyled = styled('section')`
+  height: 100%;
   display: flex;
   flex-flow: column nowrap;
   justify-content: center;
-  align-items: center;
   gap: clamp(1rem, 1vw, 2rem);
+  padding: clamp(1rem, 1vw, 2rem) clamp(1rem, 4vw, 8rem);
 `
+const ProductsButton = styled('button')`
+  color: white;
+  font-size: clamp(1rem, 1vw, 2rem);
+  font-weight: 700;
+  border-radius: 3rem;
+  background-color: black;
+  padding: clamp(1rem, 1vw, 2rem) clamp(1.5rem, 3vw, 6rem);
 
+  ${({ align }) => align && css`
+    align-self: ${align};
+  `}
+`
+const ProductsDetails = styled('div')`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 clamp(1rem, 5vw, 10rem);
+  font-size: clamp(1.2rem, 1.25vw, 2.5rem);
+  margin: clamp(.5rem, .5vw, 1rem) 0;
+`
+const ProductsTotal = styled('h2')`
+  color: grey;
+  font-weight: 600;
+`
+const ProductsPrice = styled('h3')`
+  font-weight: 700;
+`
 const Products = () => {
   return (
-    <SectionStyled>
+    <ProductsStyled>
       <Product />
       <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-    </SectionStyled>
+      <ProductsButton align='flex-end'>Empty Cart</ProductsButton>
+      <ProductsDetails>
+        <ProductsTotal>Total (12 products):</ProductsTotal>
+        <ProductsPrice>$270</ProductsPrice>
+      </ProductsDetails>
+      <ProductsButton>Go to checkout</ProductsButton>
+    </ProductsStyled>
   )
 }
 
