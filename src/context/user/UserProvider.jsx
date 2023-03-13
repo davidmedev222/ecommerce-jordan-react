419674da8bd9001c8fe63be713b2492e8f158b47
davidmedev222 <davidmedev@gmail.com>
2023-03-13 18:06:43 -0300

    chore(web): add context for functionalities with the user

--- src/context/user/UserProvider.jsx
+++ src/context/user/UserProvider.jsx
@@ -4,8 +4,39 @@ import { UserContext } from './UserContext'
 const UserProvider = ({ children }) => {
   const [user, updateUser] = useState(false)
 
+  const signUp = () => {
+    console.log('sign up')
+  }
+  const signIn = () => {
+    console.log('sign in')
+  }
+
+  const signInWithGoogle = () => {
+    console.log('sign in with google')
+  }
+  const signInWithFacebook = () => {
+    console.log('sign in with facebook')
+  }
+  const signInWithGithub = () => {
+    console.log('sign in with github')
+  }
+  const signInWithTwitter = () => {
+    console.log('sign in with twitter')
+  }
+
+  const signOut = () => {
+    console.log('sign out')
+  }
+
   const data = {
-    user
+    user,
+    signUp,
+    signIn,
+    signInWithGoogle,
+    signInWithFacebook,
+    signInWithGithub,
+    signInWithTwitter,
+    signOut
   }
 
   return <UserContext.Provider value={data}>{children}</UserContext.Provider>
