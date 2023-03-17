d1a90143a32d38c0c90aae50abab74aa5d7048a9
davidmedev222 <davidmedev@gmail.com>
2023-03-17 17:09:28 -0300

    chore(web): add file for the exports of the services

---
+++ src/services/export.jsx
@@ -0,0 +1,3 @@
+export { getProduct } from './firestore/products/getProduct'
+export { getProducts } from './firestore/products/getProducts'
+export { createUser } from './firestore/users/createUser'
