95fdd0356a48a801d1b7e08bc64cd63143020974
davidmedev222 <davidmedev@gmail.com>
2023-03-24 16:50:05 -0300

    feat(web): add loader at the order pages

--- src/pages/account/orders/OrdersPage.jsx
+++ src/pages/account/orders/OrdersPage.jsx
@@ -1,17 +1,21 @@
 import { useState, useEffect } from 'react'
 import { getOrders } from '../../../services/export'
+import { Spinner } from '../../../components/export'
 import { Orders } from './Orders'
 
 const OrdersPage = () => {
   const [data, updateData] = useState([])
+  const [loader, updateLoader] = useState(true)
 
   useEffect(() => {
     getOrders()
       .then((orders) => updateData(orders))
+      .finally(() => updateLoader(false))
   }, [])
 
   return (
     <main>
+      {loader && <Spinner />}
       <Orders data={data} />
     </main>
   )
--- src/pages/account/orders/orderDetailPage/OrderDetailPage.jsx
+++ src/pages/account/orders/orderDetailPage/OrderDetailPage.jsx
@@ -1,22 +1,25 @@
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { getOrder } from '../../../../services/export'
-import { ErrorNotFound } from '../../../../components/export'
+import { ErrorNotFound, Spinner } from '../../../../components/export'
 import { OrderDetail } from './OrderDetail'
 
 const OrderDetailPage = () => {
   const [data, updateData] = useState(null)
+  const [loader, updateLoader] = useState(true)
   const [error, updateError] = useState('')
   const { id } = useParams()
 
   useEffect(() => {
     getOrder(id)
-      .then(order => updateData(order))
-      .catch(error => updateError(error))
+      .then((order) => updateData(order))
+      .catch((error) => updateError(error))
+      .finally(() => updateLoader(false))
   }, [])
 
   return (
     <main>
+      {loader && <Spinner />}
       {error && <ErrorNotFound message={error.message} />}
       {data && <OrderDetail data={data} />}
     </main>
