b9e6c1797a4b286236113cd44731a4e0b5b272c3
davidmedev222 <davidmedev@gmail.com>
2023-03-24 16:34:00 -0300

    feat(web): add loader at the shop page

--- src/pages/shop/ShopPage.jsx
+++ src/pages/shop/ShopPage.jsx
@@ -1,19 +1,23 @@
 import { useEffect, useState } from 'react'
 import { getProducts } from '../../services/export'
+import { Spinner } from '../../components/export'
 import { Categories } from './categories/Categories'
 import { Products } from './products/Products'
 
 const ShopPage = () => {
   const [data, updateData] = useState([])
+  const [loader, updateLoader] = useState(true)
 
   useEffect(() => {
     getProducts()
       .then((products) => updateData(products))
+      .finally(() => updateLoader(false))
   }, [])
 
   return (
     <main>
       <Categories />
+      {loader && <Spinner />}
       <Products data={data} />
     </main>
   )
