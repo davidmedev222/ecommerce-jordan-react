8823a6238dbc1a0cd6100781dacb4238d5dc53ca
davidmedev222 <davidmedev@gmail.com>
2023-03-24 16:46:41 -0300

    feat(web): add loader at the user routes

--- src/routes/user/UserRouteProtected.jsx
+++ src/routes/user/UserRouteProtected.jsx
@@ -1,10 +1,11 @@
 import { useAuth } from '../../hooks/export'
 import { Navigate, Outlet } from 'react-router-dom'
+import { Spinner } from '../../components/export'
 
 const UserRouteProtected = () => {
   const { user } = useAuth()
 
-  if (user === false) return 'loading...'
+  if (user === false) return <main><Spinner /></main>
 
   if (user === null) return <Navigate to='/login' />
 
--- src/routes/user/UserRouteRedirect.jsx
+++ src/routes/user/UserRouteRedirect.jsx
@@ -1,10 +1,11 @@
 import { useAuth } from '../../hooks/export'
 import { Navigate, Outlet } from 'react-router-dom'
+import { Spinner } from '../../components/export'
 
 const UserRouteRedirect = () => {
   const { user } = useAuth()
 
-  if (user === false) return 'loading...'
+  if (user === false) return <main><Spinner /></main>
 
   if (user) return <Navigate to='/account' />
 
